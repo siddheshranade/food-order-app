86b0bd79adb52ceaee940dbf265f68bd9f2265dd
Siddhesh Ranade <ranade.siddhesh@gmail.com>
2023-06-08 19:17:36 -0400

    added header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of food!'/>
+      </div>
+    </>
+  )
+};
+
+export default Header;
